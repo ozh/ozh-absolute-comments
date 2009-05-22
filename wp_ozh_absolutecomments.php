298aa4390162f0bb9e2b39606e40882c7c5764f4
ozh <ozh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-22 11:37:06 +0000

    by_BY translation
    git-svn-id: https://plugins.svn.wordpress.org/ozh-absolute-comments/trunk@119636 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp_ozh_absolutecomments.php
+++ wp_ozh_absolutecomments.php
@@ -5,7 +5,7 @@ Plugin URI: http://planetozh.com/blog/my-projects/absolute-comments-manager-inst
 Description: Reply instantly to comments, either from the email notification, or the usual <a href="edit-comments.php">Comments</a> page, without loading the post first. <strong>For WordPress 2.5+</strong>
 Author: Ozh
 Author URI: http://planetozh.com/
-Version: 2.2.2
+Version: 2.2.2.1
 */
 
 /* Release history:
@@ -19,6 +19,7 @@ Version: 2.2.2
 		 Removed: external config file.
    2.2.1: Fixed: Strip slashes on comment prefill
    2.2.2: Fixed compat with WP 2.6
+   2.2.2.1: More translations
  */
 
 /******************************************/
